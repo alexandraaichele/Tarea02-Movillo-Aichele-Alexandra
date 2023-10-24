af18cf5f73d4ee6ca82adee607e183d5a026e529
alexandraaichele <aichelemovillo15@gmail.com>
2023-10-23 22:31:33 -0300

    Update archivo y pdf

--- OUILookup.py
+++ OUILookup.py
@@ -39,7 +39,7 @@ def obtener_tabla_arp():
         listaIpMac = []
         for linea in lineas:
             datos = re.split(r"\s+", linea.strip())
-            if len(datos[0]) > 0 and datos[0][0] in "1234567890":
+            if len(datos[0]) > 0 and datos[0][0].isnumeric():
                 ip, mac = datos[0], datos[1]
                 listaIpMac.append((ip, mac))
         
@@ -67,21 +67,23 @@ def obtener_fabricante(mac):
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "i:m:a", ["ip=", "mac=", "arp"])
+        opts = getopt.getopt(argv, "i:m:a", ["ip=", "mac=", "arp"])[0]
 
     except getopt.GetoptError:
-        print("Use: python OUILookup.py --ip <IP> | --mac <IP> | --arp | [--help]")
+        print("""Use: python OUILookup.py --ip <IP> | --mac <IP> | --arp | [--help]
+    --ip : IP del host a consultar.
+    --mac: MAC a consultar. P.e. aa:bb:cc:00:00:00.
+    --arp: muestra los fabricantes de los host disponibles en la tabla arp.
+    --help: muestra este mensaje y termina.""")
         sys.exit(2)
 
-    opt, arg = opts[0]
-    if opt in ("-i", "--ip"):
-        obtener_datos_por_ip(arg)
-    elif opt in ("-m", "--mac"):
-        obtener_datos_por_mac(arg)
-    elif opt in ("-a", "--arp"):
-        obtener_tabla_arp()
-    elif opt in ("--help"):
-        print("Use: python OUILookup.py --ip <IP> | --mac <IP> | --arp | [--help]")
+    for opt, arg in opts:
+        if opt in ("-i", "--ip"):
+            obtener_datos_por_ip(arg)
+        elif opt in ("-m", "--mac"):
+            obtener_datos_por_mac(arg)
+        else:
+            obtener_tabla_arp()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
